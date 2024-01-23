8ff2384f41ab6aae91e74e2a1c630026539d0045
Gangstaaaaa <85770558+Gangstaaaaa@users.noreply.github.com>
2024-01-23 12:15:05 -0500

    Update printer.lua

--- printer.lua
+++ printer.lua
@@ -1,9 +1,9 @@
-print(GetResourceState()"Has Started")
+print(GetResourceState()" Has Started")
 
 SetTimeout( 1000, function ()
     if GetResourceState('dpemotes') == 'started' then
         for i = 1, 10 do
-            print("RpEmotes: Warning! dpemotes is on the server, this WILL cause issues.")
+            print(GetResourceState()" Warning! dpemotes is on the server, this WILL cause issues.")
         end
     end
 end)
